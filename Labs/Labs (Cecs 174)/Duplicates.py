7993e77173be8889ffc9412231044ceeb2f081f7
Nabiha B <47676025+nabiha-b@users.noreply.github.com>
2020-06-01 21:48:50 -0700

    Update and rename Lab 7.py to Duplicates.py

--- Labs/Labs (Cecs 174)/Duplicates.py
+++ Labs/Labs (Cecs 174)/Duplicates.py
@@ -1,10 +1,11 @@
+# Nabiha Bashir
+# This program returns the sum of a list of randomly generated numbers and duplicates if they exist.
+
 #import randint function from random library
 from random import randint
-
 #Create empty list 'list_nums'
 list_nums = []
-
-#Assisgn a variable to total sum 
+#Assisgn a variable to total sum
 total_sum = 0
 
 #Use for loops to add 10 random numbers to list 'list_nums'
@@ -13,18 +14,15 @@ for i in range (10):
     x = randint(1, 26)
     list_nums.append(x)
     print(x, end=' ')
-print("")
 
-#Display the random generated list 
+#Display the random generated list
 print("\nList of random numbers generated:", list_nums)
-print("")
 
 #Use for loops to calculate sum of the list
 for i in list_nums:
     total_sum += i
 #Display total sum
 print("Total Sum:", total_sum)
-print("")
 
 #Use for loop to identify unique numbers in list and remove them
 for x in set(list_nums):
@@ -32,8 +30,3 @@ for x in set(list_nums):
 duplicates = list(set(list_nums))
 #Display the duplicates
 print("Duplicates in list: ", duplicates)
-
-
-
-
-
